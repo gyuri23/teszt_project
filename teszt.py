71d571207306c83412fdc67d7fd907dc2cb8424e
gyuri23 <dora.gyorgy@biztos-it.hu>
2016-11-11 14:52:09 +0100

    alakul

--- teszt.py
+++ teszt.py
@@ -7,12 +7,14 @@ def password_input():
     pwd_window.geometry('+550+150')
     plabel = tkinter.Label(pwd_window, text='Jelszó:').pack()
     pinput = tkinter.Entry(pwd_window, textvariable=pwd, show='*').pack()
+
     pbutton = tkinter.Button(pwd_window, text="Ok",
                              command=lambda: password_ok(pwd_window)).pack()
+    pwd_window.bind('<Return>', lambda event: password_ok(pwd_window))
 
 def password_ok(pwd_window):
-    # plabel = tkinter.Label(main_window, textvariable=pwd)
-    # plabel.grid(row=2, column=2)
+    plabel = tkinter.Label(main_window, textvariable=pwd)
+    plabel.grid(row=2, column=2)
     password_label = tkinter.Message(main_window, text='Jelszó állapot',
                                      bg='yellow', bd=3, relief='groove',
                                      width=80)
@@ -27,15 +29,12 @@ main_window.geometry('300x500+500+100')
 password_button = tkinter.Button(main_window, text="Jelszó",
                                  command=password_input)
 password_button.grid(row=1, column=0, sticky='w')
-# password_button.pack()
 
 connect_button = tkinter.Button(main_window, text="Kapcsolódás")
 connect_button.grid(row=2, column=0, sticky='w')
-# connect_button.pack()
 
 refresh_button = tkinter.Button(main_window, text="Frissítés")
 refresh_button.grid(row=3, column=0, sticky='w')
-# refresh_button.pack()
 
 password_label = tkinter.Message(main_window, text='Jelszó állapot', bg='red',
                                  bd=3, relief='groove', width=80)
