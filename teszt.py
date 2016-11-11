8aad96dc32af17fcd919ee1b9a80690cbe9c0532
gyuri23 <dora.gyorgy@biztos-it.hu>
2016-11-11 12:55:55 +0100

    alakul

--- teszt.py
+++ teszt.py
@@ -2,11 +2,30 @@
 import tkinter
 # from tkinter import messagebox
 
-main_window = tkinter.Tk()      # http://effbot.org/tkinterbook/wm.htm
-main_window.title('Ablak neve')
-main_window.geometry('300x500+500+100') #széles x magas + bal elűről jobbra + bal felűről lefelé
+def password_input():
+    pwd_window = tkinter.Toplevel(main_window)
+    pwd_window.geometry('+550+150')
+    plabel = tkinter.Label(pwd_window, text='Jelszó:').pack()
+    pinput = tkinter.Entry(pwd_window, textvariable=pwd, show='*').pack()
+    pbutton = tkinter.Button(pwd_window, text="Ok",
+                             command=lambda: password_ok(pwd_window)).pack()
 
-password_button = tkinter.Button(main_window, text="Jelszó")
+def password_ok(pwd_window):
+    # plabel = tkinter.Label(main_window, textvariable=pwd)
+    # plabel.grid(row=2, column=2)
+    password_label = tkinter.Message(main_window, text='Jelszó állapot',
+                                     bg='yellow', bd=3, relief='groove',
+                                     width=80)
+    password_label.grid(row=0, column=1)
+    pwd_window.destroy()
+
+main_window = tkinter.Tk()
+pwd = tkinter.StringVar()
+main_window.title('Ablak neve')
+main_window.geometry('300x500+500+100')
+
+password_button = tkinter.Button(main_window, text="Jelszó",
+                                 command=password_input)
 password_button.grid(row=1, column=0, sticky='w')
 # password_button.pack()
 
@@ -17,7 +36,6 @@ connect_button.grid(row=2, column=0, sticky='w')
 refresh_button = tkinter.Button(main_window, text="Frissítés")
 refresh_button.grid(row=3, column=0, sticky='w')
 # refresh_button.pack()
-
 
 password_label = tkinter.Message(main_window, text='Jelszó állapot', bg='red',
                                  bd=3, relief='groove', width=80)
